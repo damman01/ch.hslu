4dc8c9764f1bf4d6935609e791353074cb88f3d5
Dominic Ammann <dominic.ammann@hslu.ch>
2021-06-21 19:31:56 +0200

    added week 11

--- src/main/java/ch/hslu/sw11/Switchable.java
+++ src/main/java/ch/hslu/sw11/Switchable.java
@@ -1,5 +1,33 @@
+/**
+ * Dieses Interface implementiert einen Schalter
+ * 
+ * @autor Dominic Ammann
+ */
 package ch.hslu.sw11;
 
-public class Switchable {
+public interface Switchable {
 
+    /**
+     * Stellt den Schalter auf ON
+     */
+    void switchon();
+
+    /**
+     * Stellt den Schalter auf OFF
+     */
+    void switchoff();
+
+    /**
+     * Prüft ob der Schalter auf ON ist
+     * 
+     * @return TRUE wenn Schalter = ON
+     */
+    boolean isSwitchedOn();
+
+    /**
+     * Prüft ob der Schalter auf OFF ist
+     * 
+     * @return TRUE wenn Schalter = OFF
+     */
+    boolean isSwitchedOff();
 }
