97d61abbb205e226968db1334749e399a380f57f
Dominic Ammann <dominic.ammann@stud.hslu.ch>
2021-10-05 12:19:19 +0200

    Added Classes for Week 3

---
+++ src/main/java/ch/hslu/ad/sw03/BinaryTree.java
@@ -0,0 +1,23 @@
+package ch.hslu.ad.sw03;
+
+public class BinaryTree<T> implements Tree<T> {
+
+    @Override
+    public int compareTo(T o) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public void add(T elemenT) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void remove(T elementT) {
+        // TODO Auto-generated method stub
+        
+    }
+    
+}
---
+++ src/main/java/ch/hslu/ad/sw03/Tree.java
@@ -0,0 +1,8 @@
+package ch.hslu.ad.sw03;
+
+public interface Tree<T> extends Comparable<T>{
+    
+    public void add(T elemenT) ;
+
+    public void remove(T elementT);
+}
